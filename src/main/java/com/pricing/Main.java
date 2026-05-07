4efc01b7c75c4be6dd0b4d9460a64c5cb30829a8
saadia <saadiabenkadi@gmail.com>
2026-05-07 23:52:03 +0200

    feat: initialize Gradle app

---
+++ src/main/java/com/pricing/Main.java
@@ -0,0 +1,8 @@
+package com.pricing;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Pricing & Discount Engine Lab");
+        System.out.println("Run tests with: ./gradlew test");
+    }
+}
